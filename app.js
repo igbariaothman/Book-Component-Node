459fd2fa8568b01a45c8696b524fdacc01a8e373
igbariaothman <igbariaothman@gmail.com>
2025-11-17 19:55:52 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// 327809190 עותמאן אגבאריה
+//   214626913 אחמד חבשי
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = 3000;
+
+const htmlPath = path.join(__dirname, "templates", "page.html");
+const server = http.createServer((req, res) => {
+  fs.readFile(htmlPath, "utf8", (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+      return res.end("Server error");
+    }
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end(data);
+  });
+});
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost: ${port}`);
+});
